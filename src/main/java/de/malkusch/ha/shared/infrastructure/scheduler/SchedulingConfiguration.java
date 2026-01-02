8a1ecfd5075f2bf2dfabfee93aeb76d494f1f08c
Markus-Malkusch <markus.malkusch@ibm.com>
2026-01-02 19:22:21 +0100

    Reduce thread count

--- src/main/java/de/malkusch/ha/shared/infrastructure/scheduler/SchedulingConfiguration.java
+++ src/main/java/de/malkusch/ha/shared/infrastructure/scheduler/SchedulingConfiguration.java
@@ -1,22 +1,26 @@
 package de.malkusch.ha.shared.infrastructure.scheduler;
 
-import static java.util.concurrent.Executors.newScheduledThreadPool;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.concurrent.Executors.newScheduledThreadPool;
+
 @Configuration
 @EnableScheduling
 class SchedulingConfiguration implements SchedulingConfigurer {
+
+    @Value("${scheduler.pool-size}")
+    private int poolSize;
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         var counter = new AtomicInteger();
         taskRegistrar.setScheduler(
-                newScheduledThreadPool(10, it -> new Thread(it, "@Scheduled-" + counter.incrementAndGet())));
+                newScheduledThreadPool(poolSize, it -> new Thread(it, "@Scheduled-" + counter.incrementAndGet())));
     }
 }
